c74e5edd58407c4198818972a89c0d97d530d92a
Dmitriy <soapman@bk.ru>
2022-02-10 19:04:14 +0300

    Initial commit

---
+++ Source/TinybuildTest/TinybuildTestGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TinybuildTestGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TINYBUILDTEST_API ATinybuildTestGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
